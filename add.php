380cc9aaf8efc152d7f9bb7a239bf1af5ed0c105
wejdanhas <wejdanbabatain@yahoo.com>
2017-03-13 22:55:24 -0400

    Update add.php

--- add.php
+++ add.php
@@ -2,40 +2,60 @@
 
 <html>
 
-<head>
+    <head>
+        <title>View Records</title>
 
-<title>View Records</title>
+        <link rel="stylesheet" type="text/css" href="webStyle.css">    
+    </head>
 
-</head>
+    <body>
+        <div id="container"> 
+            <div id="top">
+                <br/>
+                <br/>
+                <br/>
+                <h2>Drexel Library Website </h2>
+            </div>
 
-<body>
+            <div class="navbar">
 
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "User_info";
+                <a href="home.html">Home</a>
+                <a href="view.php">Borrowed Book list</a>
+                <a href="#contact">Contact</a>
+                <a href="#about">About</a>
 
+            </div>
+            <br/>
+
+        </div> 
+        <div class="wrap">
+
+
+            <?php
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "User_info";
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+            $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-$User_ID= 7;
-$User_Name= "X6";
-$User_Email= "k8666@drexel.edu";
-$Book_title= "Gooo";
-$sql = "INSERT INTO Users (User_ID, User_Name, User_Email,Book_title)
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+
+
+            $User_ID = $_POST["userID"];
+            $User_Name = $_POST["userName"];
+            $User_Email = $_POST["userEmail"];
+            $Book_title = $_POST['title'];
+            $sql = "INSERT INTO Users (User_ID, User_Name, User_Email,Book_title)
 VALUES ('$User_ID', '$User_Name', '$User_Email','$Book_title')";
-
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-$conn->close();
-?>
- </body>
+            if ($conn->query($sql) === TRUE) {
+                echo "New record created successfully";
+            } else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+            $conn->close();
+            ?>
+    </body>
 </html>
