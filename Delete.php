4b0d9188f3407b2ffbb546d0919c83fab16bd3c6
wejdanhas <wejdanbabatain@yahoo.com>
2017-03-13 22:42:51 -0400

    Update Delete.php

--- Delete.php
+++ Delete.php
@@ -1,40 +1,62 @@
-
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 
 <html>
 
-<head>
+    <head>
 
-<title>View Records</title>
+        <title>View Records</title>
+        <link rel="stylesheet" type="text/css" href="webStyle.css">    
+    </head>
 
-</head>
+    <body>
+        <div id="container"> 
+            <div id="top">
+                <br/>
+                <br/>
+                <br/>
+                <h2>Drexel Library Website </h2>
+            </div>
 
-<body><?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "User_info";
+            <div class="navbar">
 
+                <a href="home.html">Home</a>
+                <a href="view.php">Borrowed Book list</a>
+                <a href="#contact">Contact</a>
+                <a href="#about">About</a>
+
+            </div>
+            <br/>
+
+        </div> 
+        <div class="wrap">
+
+
+            <?php
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "User_info";
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+            $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
 
-// sql to delete a record
-//$User_Name= $_POST("User_Name");
-//$User_ID= $_POST["User_ID"];
-$User_ID=1;
-$sql = "DELETE FROM Users WHERE User_ID='$User_ID'";
+            $User_ID = $_GET['User_ID'];
 
-if ($conn->query($sql) === TRUE) {
-    echo "Record deleted successfully";
-} else {
-    echo "Error deleting record: " . $conn->error;
-}
 
-$conn->close();
-?>
-</body>
+
+            $sql = "DELETE FROM Users WHERE User_ID=$User_ID";
+            if ($conn->query($sql) === TRUE) {
+                echo "Record deleted successfully";
+                echo $User_ID;
+            } else {
+                echo "Error deleting record: " . $conn->error;
+            }
+            $conn->close();
+            ?>
+
+        </div>
+    </body>
 </html>
