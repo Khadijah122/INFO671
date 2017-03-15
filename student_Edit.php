07d9b5aeeba2e7267b57a36d9878e7d67e9cf484
wejdanhas <wejdanbabatain@yahoo.com>
2017-03-15 16:13:28 -0400

    Update student_Edit.php

--- student_Edit.php
+++ student_Edit.php
@@ -26,8 +26,8 @@ and open the template in the editor.
 
                 <a href="index.html">Home</a>
                 <a href="view.php">Borrowed Book list</a>
-                <a href="#contact">Contact</a>
-                <a href="#about">About</a>
+                <a href="contact.html">Contact</a>
+            <a href="about.html">About</a>
 
             </div>
             <br/>
@@ -41,83 +41,95 @@ and open the template in the editor.
 
 
         <script>
+           
             function ValidateForm() {
+
                 var hasError = false;
+                //.search(/^[a-zA-Z]+$/)
                 if (document.getElementById('lname').value === "") {
                     document.getElementById('wronglname').style.display = "inline";
                     hasError = true;
                 } else {
-                    document.getElementById('wrongname').style.display = "none";
+                    document.getElementById('wronglname').style.display = "none";
                 }
-                if (document.getElementById('id').value.search(/^[0-9]+$/)) {
+
+                if (document.getElementById('id').value.search(/^[0-9]+$/)=== -1) {
                     document.getElementById('wrongid').style.display = "inline";
                     hasError = true;
                 } else {
                     document.getElementById('wrongid').style.display = "none";
                 }
+
                 if (document.getElementById('email').value.search(/^[a-zA-Z]+([_\.-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9]+([\.-]?[a-zA-Z0-9]+)*(\.[a-zA-Z]{2,4})+$/) === -1) {
                     document.getElementById('wrongemail').style.display = "inline";
                     hasError = true;
                 } else {
                     document.getElementById('wrongemail').style.display = "none";
                 }
+
                 return !hasError;
             }
-            document.getElementById('test').onsubmit = ValidateForm;
+            document.getElementById('test').onsubmit = "return ValidateForm()";
+        
         </script>
 
         <div class="wrap">
-            <h2>Please enter the student details here:</h2> 
-            <?php
-            $User_ID = $_GET['User_ID'];
+             <div class="inner-container">
 
-            $servername = "localhost";
+                    <div class="box">
+
+            <h2>Please enter the student details here:</h2> 
+          
+
+            <form id='test' action = "edit.php" method = "post" onSubmit="return ValidateForm()">
+               
+           <?php $User_ID = $_GET['User_ID'];
+           $servername = "localhost";
             $username = "root";
             $password = "";
             $dbname = "User_info";
-// Create connection
+            
+            // Create connection
             $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
             if ($conn->connect_error) {
                 die("Connection failed: " . $conn->connect_error);
             }
-
             $sql = "SELECT * FROM Users WHERE User_ID='$User_ID'";
             $stmt = $conn->prepare($sql);
 //$stmt->bind_param("s", $editID);
             $stmt->execute();
             $result = $stmt->get_result();
             $row = $result->fetch_assoc();
-
-           
-            ?>
-
-            <form id='test' action = "Edit.php" method = "post">
-
-               User_ID:
+                  ?> 
+                
+                
+                <h4>  User_ID:
             <?php
             $User_ID = $_GET["User_ID"];
             echo $User_ID;
-            ?>
-
-                <input type  ="hidden" name="User_ID" value="<?php echo $_GET['User_ID'] ?>"> <br/><br/>
+            ?></h4> <input type  ="hidden" name="User_ID" value="<?php echo $_GET['User_ID'] ?>"> 
                
                 Book Title: 
-                <input type  ="text" name="Book_title" value="<?php echo $row['Book_title'] ?>"><br/><br/>
+                &nbsp;&nbsp; <input type  ="text" name="Book_title" value="<?php echo $row['Book_title'] ?>"><br/><br/>
 
-                User_Name:
+                User Name:
                 <input name= "User_Name"  type="text" size="12" value="<?php echo $row['User_Name'] ?>"><span id="wrongid" class="error">*This is a required field</span> <br/><br/>
-                Email :
-                <input name= "User_Email"  type="text" value="<?php echo $row['User_Email'] ?>"><span id="wrongemail" class="error">* Wrong Email Address</span> <br/><br/>
+                User Email:
+            &nbsp;<input name= "User_Email"  type="text" value="<?php echo $row['User_Email'] ?>"><span id="wrongemail" class="error">* Wrong Email Address</span> <br/><br/>
 
-                <div>
-                    <input type="submit" value="Submit">
-                </div>
+                
+                
+                    <input type="submit" value="Next">
+                
+                
             </form>
 
-            <div><button type="button" onclick="index.html">Previous</button>
-                <button type="button" onclick="alert('code target here')">Next</button></div>
+           
         </div>
+             </div>
+            </div>
+   
 
     </body>
 </html>
