cba2acb9691dc28d3182249ae1277e726094f7b2
wejdanhas <wejdanbabatain@yahoo.com>
2017-03-15 16:06:01 -0400

    Update Edit.php

--- Edit.php
+++ Edit.php
@@ -8,96 +8,18 @@ and open the template in the editor.
 -->
 <html>
     <head>
+       
+        <title>Edit User Information </title>
         <meta charset="UTF-8">
-        <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <link rel="stylesheet" type="text/css" href="webStyle.css"> 
-        <style>
-            body{
-                padding:0;
-                margin:0;
-            }
-            .vid-container{
-                position:relative;
-                height:100vh;
-                overflow:hidden;
-            }
-            .bgvid{
-                position:absolute;
-                left:0;
-                top:0;
-                width:100vw;
-            }
-            .inner-container{
-                width:400px;
-                height:400px;
-                position:absolute;
-                top:calc(50vh - 200px);
-                left:calc(50vw - 200px);
-                overflow:hidden;
-            }
-            .bgvid.inner{
-                top:calc(-50vh + 200px);
-                left:calc(-50vw + 200px);
-                filter: url("data:image/svg+xml;utf9,<svg%20version='1.1'%20xmlns='http://www.w3.org/2000/svg'><filter%20id='blur'><feGaussianBlur%20stdDeviation='10'%20/></filter></svg>#blur");
-                -webkit-filter:blur(10px);
-                -ms-filter: blur(10px);
-                -o-filter: blur(10px);
-                filter:blur(10px);
-            }
-            .box{
-                position:absolute;
-                height:100%;
-                width:100%;
-                font-family:Helvetica;
-                color:#fff;
-                background:rgba(0,0,0,0.13);
-                padding:30px 0px;
-            }
-            .box h2{
-                text-align:center;
-                margin:30px 0;
-                font-size:30px;
-            }
-            .box input{
-                display:block;
-                width:300px;
-                margin:20px auto;
-                padding:15px;
-                background:rgba(0,0,0,0.2);
-                color:#fff;
-                border:0;
-            }
-            .box input:focus,.box input:active,.box button:focus,.box button:active{
-                outline:none;
-            }
-            .box button{
-                background:#2ecc71;
-                border:0;
-                color:#fff;
-                padding:10px;
-                font-size:20px;
-                width:330px;
-                margin:20px auto;
-                display:block;
-                cursor:pointer;
-            }
-            .box button:active{
-                background:#27ae60;
-            }
-            .box p{
-                font-size:14px;
-                text-align:center;
-            }
-            .box p span{
-                cursor:pointer;
-                color:#666;
-            }
-        </style>
+       
+            
     </head>
-    <body  background=86132.jpg>
+    <body >
 
-        <div id="container"> 
+        
             <div id="top">
                 <br/>
                 <br/>
@@ -109,15 +31,15 @@ and open the template in the editor.
 
                 <a href="index.html">Home</a>
                 <a href="view.php">Borrowed Book list</a>
-                <a href="#contact">Contact</a>
-                <a href="#about">About</a>
+                <a href="contact.html">Contact</a>
+            <a href="about.html">About</a>
 
             </div>
             <br/>
 
-        </div> 
+       
         <div class="wrap">
-            <div class="vid-container">
+            
 
                 <div class="inner-container">
 
@@ -151,7 +73,8 @@ and open the template in the editor.
 
 
 
-                            echo "<center> <h2> <br> <br>Record updated successfully </h2></center>";
+                                       echo "<center> <h2> <br> <br>Record with the ID " . $User_ID . " has been edited successfully </h2></center>";
+
                         } else {
                             echo "Error updating record: " . $conn->error;
                         }
@@ -162,7 +85,7 @@ and open the template in the editor.
                     </div>
                 </div>
             </div>
-        </div>
+        
 
     </body>
 </html>
